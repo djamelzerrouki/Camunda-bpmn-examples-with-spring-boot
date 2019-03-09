85768d93b11dda8ab709d6ba6e4a0295ef333f25
zerrouki djamel <djameljimmizerrouki@gmail.com>
2019-03-09 20:59:25 +0100

    adding updateEmploye

--- src/main/java/com/example/ControllerRepcartenational.java
+++ src/main/java/com/example/ControllerRepcartenational.java
@@ -15,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,7 +37,7 @@ public class ControllerRepcartenational {
 	private   RepoEmployecartenational red ;
 	@Autowired 
 	private   RepoServicecartenational rsd ;
- 	private static String namedb; 
+	private static String namedb; 
 	public static String uploadDirectory = System.getProperty("user.dir")+"/uploads";
 
 	// Modele selectioner
@@ -53,10 +55,10 @@ public class ControllerRepcartenational {
 	@RequestMapping(value="/Employe" ,method=RequestMethod.GET)
 	public String formEmploye(Model model) {
 		List<Employe> list = red.findAll();
-		 model.addAttribute("services", rsd.findAll());
+		model.addAttribute("services", rsd.findAll());
 		model.addAttribute("enployee",new Employe());
 		model.addAttribute("enployees",list);
- 		return "addenployee";
+		return "addenployee";
 	}
 	//Service
 	@RequestMapping(value="/Service" ,method=RequestMethod.GET)
@@ -70,9 +72,9 @@ public class ControllerRepcartenational {
 	@RequestMapping(value="/saveEnployee" ,method=RequestMethod.POST)
 	public String saveEnployee(Model model, @Valid @ModelAttribute("enployee")Employe ep, BindingResult result) {
 		System.out.println("### :->"+ep.getService());
-//Service service = new Service();
-//service.setId(5);
-	//	ep.setService(service);
+		//Service service = new Service();
+		//service.setId(5);
+		//	ep.setService(service);
 		red.save(ep);
 		return "redirect:Employe";
 	}	
@@ -102,21 +104,21 @@ public class ControllerRepcartenational {
 				//Config.configAll(namemodel,index);
 				red.deleteAll();
 				rsd.deleteAll();
-			
+
 				Collection<Task> tasks =testcamanda.mymethod(fileNames.toString());
-				
+
 				tasks.forEach(t->{
-			
+
 					// read attributes bvars.entrySet()y helper methods
 					String id = t.getId();
 					String name = t.getName();
 					String type=t.getElementType().getTypeName();
 					Service srv =new Service();
- 					srv.setIdtask(id);
+					srv.setIdtask(id);
 					srv.setName(name);
 					srv.setType(type);
 
-					 
+
 					List<Task> possibleTasks=	new testcamanda().getNextTasks(t.getId() ,fileNames.toString());
 
 					System .out.println("ID: "+ id +" Name : "+name +" Type : "+type);
@@ -125,7 +127,7 @@ public class ControllerRepcartenational {
 
 						System .out.println("NEXT TASK -> ID: "+ tsk.getId() +" Name : "+tsk.getName() +" Type : "+type);
 					});
-					
+
 					rsd.save(srv);
 				});
 
@@ -140,6 +142,47 @@ public class ControllerRepcartenational {
 		return "redirect:Service";
 	}
 
+ 
 
+	@PostMapping("/updateEmploye")
+	public String update(@RequestParam(name ="id") Long id, @Valid Employe employe, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			employe.setId(id);
+			return "redirect:updateEmploye";
+			
+		}
+		red.save(employe);
+		model.addAttribute("services", rsd.findAll());
+		model.addAttribute("enployee", employe);
+		model.addAttribute("enployees",red.findAll());
+		return "redirect:Employe";
+		
+	}
+	//Edit 
+	@GetMapping("/editEmploye")
+	public String showUpdateForm(@RequestParam(name ="id") Long id, Model model) {
+		Employe employe = red.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid Employe Id:" + id));
+
+
+		model.addAttribute("services", rsd.findAll());
+		model.addAttribute("enployee", employe);
+		model.addAttribute("enployees",red.findAll());
+
+		return "updatenployee";
+	}
+
+
+	//	     
+	//	@GetMapping("/delete/{id}")
+	//	public String deleteUser(@PathVariable("id") long id, Model model) {
+	//	    User user = userRepository.findById(id)
+	//	      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	//	    userRepository.delete(user);
+	//	    model.addAttribute("users", userRepository.findAll());
+	//	    return "index";
+	//	}
+	//
+	//
 
 }
