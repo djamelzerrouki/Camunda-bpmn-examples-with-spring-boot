2f13ed3fc1e8aac0a8d783e24e4f9182fe502b8c
zerrouki djamel <djameljimmizerrouki@gmail.com>
2019-03-10 02:33:19 +0100

    adding model bootsetrap save and updat employe

--- src/main/java/com/example/ControllerRepcartenational.java
+++ src/main/java/com/example/ControllerRepcartenational.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.model.Employe;
@@ -184,5 +185,10 @@ public class ControllerRepcartenational {
 	//	}
 	//
 	//
+	@GetMapping("/findemploye")
+	@ResponseBody
+	public Employe findOne(@RequestParam(name ="id") Long id) {
+		return red.getOne(id);
+	}
 
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -179,3 +179,51 @@
 
 
 }());
+//$('#exampleModal').on('show.bs.modal', function (event) {
+//	  var button = $(event.relatedTarget) // Button that triggered the modal
+//	  var recipient = button.data('whatever') // Extract info from data-* attributes
+//	  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//	  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//	  var modal = $(this)
+//	  modal.find('.modal-title').text('New message to ' + recipient)
+//	  modal.find('.modal-body #name').val('jkjkjkj')
+//	});
+
+// 
+//	$('.tabl .eBtn').on('click', function (event) {
+//		 
+//
+//		event.preventDefault();
+//		  var href = $(this).attr('href'); 
+//		  
+//		  var text = $(this).text();
+//		  if (text=='Edit') {
+//			  $.get(href,function(employe,status){
+//					$('.myForm #name').val(employe.name)
+// 
+//			  });
+//			  $('.myForm #exampleModal').modal();
+//
+//		} else {
+//			
+//			$('.myForm #name').val(employe.name)
+//			  $('.myForm #exampleModal').modal();
+//
+//		}
+//		   	});
+
+	$('.table .eBtn').on('click', function (event) {
+		 
+		  
+		 event.preventDefault();
+	  var href = $(this).attr('href'); 
+		  
+	  $.get(href,function(e,status){
+		  modal.find('.modal-body #name').val("djamel");
+
+	  });
+ 
+			  $('.myForm #exampleModal').modal();
+
+		   	});
+ 
